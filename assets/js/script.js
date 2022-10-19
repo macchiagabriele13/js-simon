27211695d28a97eb2547db4f470446e61b2aab7b
macchiagabriele13 <macchia_gabriele@libero.it>
2022-10-19 21:11:12 +0200

    inizio esercizio

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+/* Dopo 30 secondi i numeri scompaiono
+
+    Creo un comando asincrono che faccia scomparire dopo 30 secondi il paragrafo  */
+
+setTimeout(blackout, 30000);
+
+
+/* Visualizzare in pagina 5 numeri casuali. */
+
+    /* Utilizzo la funzione math per creare 5 numeri random */
+
+    let randomNumber = Math.floor((Math.random() * 100) + 1);
+    console.log(randomNumber);
+
+   /*  Poi creo una variabile e li inserisco dentro un paragrafo tramite la classe numeri */
+
+
+/* Da lì parte un timer di 30 secondi. */
+
+    /* Creo un timer tramite una funzione time  */
+
+
+/* Dopo 30 secondi i numeri scompaiono */
+function blackout(parametro) {
+    
+}
+
+/* e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). */
+    
+  /*   Creo un prompt e lo inserisco in un ciclo per mostrarlo 5 volte */
+
+
+/* Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
