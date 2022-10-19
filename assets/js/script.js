f90f43995fa933ac2e9b412e37550bd82ac837ec
macchiagabriele13 <macchia_gabriele@libero.it>
2022-10-19 23:59:47 +0200

    ex - fino a prima del confronto

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,86 +14,90 @@ let number = document.querySelector(".number")
 console.log(number);
 
 
-/* Inserisco variabile RandomNumber per Array */
-let randomNumbers = []
 
 /* Aggiungo un addEventListener al click sul bottone  */
 
-bottoneGeneratore.addEventListener('click', function () {
+/* bottoneGeneratore.addEventListener('click', function () {
 
     numberBox.innerHTML = ''
     console.log("Bottone cliccato");
 
     numberBox.style.display = 'flex'
 
-    /* Dopo 30 secondi i numeri scompaiono
     
-        Creo un comando asincrono che faccia scomparire dopo 30 secondi il paragrafo  */
+/* })  */
 
-    setTimeout(blackout, 30000);
+/* /* Visualizzare in pagina 5 numeri casuali. */
 
-
-    /* Visualizzare in pagina 5 numeri casuali. */
-
-    /* Utilizzo la funzione math per creare 5 numeri random */
-    /*  Poi creo una variabile e li inserisco dentro un paragrafo tramite la classe numeri */
-
-    
-   /*  randomNumbers(0, 100)
-
-    function randomNumbers(min, max) {
-        const numbersEl = document.querySelector('.number')
-        
-        let numbersArray = []
-        for (i = 0; i < 5; i++) {
-            const random = Math.floor(Math.random() * (max - min + 1)) + min;
-            numbersArray.push(random)
-            /* console.log(random) */
-        /* } */
-        /* console.log(numbersArray);
-        numbersEl.innerHTML = numbersArray */
-
-        /* const string = numbersArray.join(' ')
-        numbersEl.innerHTML = `${string}` */
-  /*   } */
-    /* return randomNumbers */
+/* Utilizzo la funzione math per creare 5 numeri random */
+/*  Poi creo una variabile e li inserisco dentro un paragrafo tramite la classe numeri */
 
 
+function randomNumbers(min, max) {
+    const numbersEl = document.querySelector('.number')
 
-    for(let i = 0; i < 5; i++){
-        number = Math.floor(Math.random()*30)
-        randomNumbers.push(number)
-        console.log(randomNumbers)
+    let numbersArray = []
+    for (i = 0; i < 5; i++) {
+        const random = Math.floor(Math.random() * (max - min + 1)) + min;
+        numbersArray.push(random)
+        console.log(random)
     }
-    
-    number.innerHTML = randomNumbers
+    console.log(numbersArray);
+    numbersEl.innerHTML = numbersArray
 
+    const string = numbersArray.join(' ')
+    numbersEl.innerHTML = `${string}`
+}
 
- }) 
-
-
-
-
-
-
-
-
-
+randomNumbers(0, 100)
 
 
 /* Da lì parte un timer di 30 secondi. */
 
 /* Creo un timer tramite una funzione time  */
 
+/* Dopo 30 secondi i numeri scompaiono
+    
+        Creo un comando asincrono che faccia scomparire dopo 30 secondi il paragrafo  */
+
+let seconds = 30
+const intervalId = setInterval(countdown, 1000)
+
+        setTimeout(timeClear, 30001)
+        setTimeout(timeMemory, 30002);
+
+function countdown() {
+      document.querySelector('.counter').innerText = seconds;
+      if (seconds == 0){
+            /* numberCheck(arrayNumber, arrayNumberUser); */
+           seconds.innerHTML = ('finito')
+            
+      } else {
+            seconds--
+      }
+}
+
 
 /* Dopo 30 secondi i numeri scompaiono */
-function blackout(parametro) {
 
+
+function timeClear() {
+    const numbersEl = document.querySelector('.number_box')
+    numbersEl.classList.add('d-none')
 }
 
 /* e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). */
 
 /*   Creo un prompt e lo inserisco in un ciclo per mostrarlo 5 volte */
+
+function timeMemory() {
+    const userNumbersArray = []
+    for (i = 0; i < 5; i++) {
+        const UserMemoryNumber = Number((prompt('inserisci il numero che ti ricordi')))
+        userNumbersArray.push(UserMemoryNumber)
+    }
+    console.log(userNumbersArray);
+}
 
 
 /* Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
